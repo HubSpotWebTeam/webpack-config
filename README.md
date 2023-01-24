9d258858547535df5a21c20d529dfd0cfb509a86
David <2527460+david-mooney@users.noreply.github.com>
2023-01-24 13:24:37 +0000

    initial repo setup

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # webpack-config
+
 A shared Webpack configuration
+
+## Installation
+
+```bash
+npm install --save-dev @hs-web-team/webpack-config
+```
+
+## Usage
+
+```js
+const { cmsConfig, cmsConfigDev } = require('@hs-web-team/webpack-config');
+```
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { default as cmsConfig } from './cms/cms.config';
+
+export { default as cmsConfigDev } from './cms/cms.config.dev';
+
+export { default as reactConfig } from './react/react.config';
+
+export { default as ReactConfigDev } from './react/react.config.dev';
